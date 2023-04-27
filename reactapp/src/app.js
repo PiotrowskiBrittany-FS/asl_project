2db97483163e47849884a8e234a86d8e9b7f3bd2
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-04-27 15:30:31 -0400

    Created react application. Redone due to error

--- reactapp/src/app.js
+++ reactapp/src/app.js
@@ -1,72 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState} from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes
-} from 'react-router-dom';
-import Home from './pages/Home'
-import Quiz from './pages/Quiz'
-import Header from './component/Header';
-import Login from './pages/Login'
-import queryString from 'querystring';
-import axios from 'axios'
-import Logout from './pages/Logout';
 
-
-
-const App = () => {
-  const [token, setToken] = useState('')
-  useEffect(() => {
-    async function fetchToken() {
-      const params = queryString.parse(window.location.search.replace(/^\?/, ''))
-      localStorage.token = params.token
-      const response = await axios('http://localhost:3000/auth/token/', {
-        headers: {
-          token: localStorage.token
-        }
-      })
-      setToken(response.data.token)
-    }
-    fetchToken()
-  }, []);
-
-  if (!token) {
-    return (
-      <div> 
-        <div style={styles.headerStyle}>
-          <h1 style={styles.h1}>Quizzes on Demand</h1>
-        </div>
-        <Login />
-      </div> 
-    )
-  }
-
+function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header token/>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/quizzes/:id' element={<Quiz />} />
-            <Route exact path='/logout' element={<Logout />} />
-          </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 export default App;
-
-const styles = {
-  headerStyle: {
-      backgroundColor: '#0B2027',
-      marginBottom: '20px'
-  },
-  h1: {
-      padding: '1rem 0',
-      textAlign: 'center',
-  fontSize: '6rem',
-      color: '#DFF0F6'
-}
-}
