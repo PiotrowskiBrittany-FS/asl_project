ba6c5958d1418f5449c001e3b0e25c006a6b1936
PiotrowskiBrittany-FS <bepiotrowski@student.fullsail.edu>
2023-04-30 18:42:21 -0400

    Final attempt to fix errors. Login is causing useRef break.

--- reactapp/src/app.js
+++ reactapp/src/app.js
@@ -1,34 +1,37 @@
-import './App.css';
-import React, { useEffect, useState} from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Nav from './components/Nav'
-import Home from './pages/Home';
-import Quiz from './pages/Quiz';
-import Header from './components/Header';
-import Login from './pages/Login';
-import QuizLayout from './pages/QuizLayout';
-import queryString from 'querystring';
-import axios from 'axios'
-import Logout from './pages/Logout';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Quizzes from "./pages/QuizLayout";
+import queryString from "querystring";
+import Login from "./pages/Login"
 
 const App = () => {
-  const [token, setToken] = useState('')
+  const [jwt, setJwt] = useState("");
   useEffect(() => {
-    async function fetchToken() {
-      const params = queryString.parse(window.location.search.replace(/^\?/, ''))
-      localStorage.token = params.token
-      const response = await axios('http://localhost:3000/auth/token/', {
+    async function fetchJwt() {
+      const params = queryString.parse(
+        window.location.search.replace(/^\?/, "")
+      );
+      console.log(localStorage.token);
+      console.log(params.token);
+      localStorage.token = params.token;
+      console.log(localStorage.token);
+      console.log(params.token);
+      const response = await axios("http://localhost:3000/auth/token/", {
         headers: {
-          token: localStorage.token
-        }
-      })
-      setToken(response.data.token)
+          token: localStorage.token,
+        },
+      });
+      setJwt(response.data.token);
     }
-    fetchToken()
+    fetchJwt();
   }, []);
 
-  if (!token) {
+
+  if (!jwt) {
     return (
       <div> 
         <div style={styles.headerStyle}>
@@ -41,21 +44,19 @@ const App = () => {
 
   return (
     <Router>
-    <div>
-      <Nav isLoggedIn={token ? true : false} />
-    </div>
       <div>
-        <Header token/>
+        <Nav isLoggedIn={jwt ? true : false} />
+        <div>
           <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/quizzes/:id' element={<Quiz />} />
-            <Route exact path='/quizlayout' element={<QuizLayout />}/>
-            <Route exact path='/logout' element={<Logout />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/QuizLayout" element={<Quizzes />} />
+            <Route exact path="/quizzes/:id" element={<Quiz />} />
           </Routes>
+        </div>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
 
@@ -71,3 +72,6 @@ const styles = {
       color: '#000'
 }
 }
+
+
+
